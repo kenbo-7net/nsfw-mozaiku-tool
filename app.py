11b841d120975a948d2a962a3891cbe3184e81e7
kenbo-7net <104003646+kenbo-7net@users.noreply.github.com>
2025-07-17 20:20:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,40 +12,37 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
-# ルートページ
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# POST処理
-@app.route('/upload', methods=['POST'])
-def upload():
-    # アップロードフォルダ初期化
+@app.route('/process', methods=['POST'])
+def process():
+    # フォルダを初期化
     for folder in [UPLOAD_FOLDER, OUTPUT_FOLDER]:
-        if os.path.exists(folder):
-            for f in os.listdir(folder):
-                os.remove(os.path.join(folder, f))
-        else:
-            os.makedirs(folder)
+        os.makedirs(folder, exist_ok=True)
+        for f in os.listdir(folder):
+            os.remove(os.path.join(folder, f))
 
-    # ファイル保存
+    # パラメータ取得
+    mosaic_size = int(request.form.get('mosaic_size', 30))
+
+    # 画像保存
     files = request.files.getlist('images')
     for file in files:
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
 
-    # モザイク処理
-    process_images(UPLOAD_FOLDER, OUTPUT_FOLDER)
+    # モザイク処理（オプション付き）
+    process_images(UPLOAD_FOLDER, OUTPUT_FOLDER, mosaic_size=mosaic_size)
 
     # ZIP圧縮
     with zipfile.ZipFile(ZIP_PATH, 'w') as zipf:
         for fname in os.listdir(OUTPUT_FOLDER):
-            fpath = os.path.join(OUTPUT_FOLDER, fname)
-            zipf.write(fpath, arcname=fname)
+            zipf.write(os.path.join(OUTPUT_FOLDER, fname), arcname=fname)
 
     return redirect('/download')
 
-# ダウンロード用ルート
 @app.route('/download')
 def download():
     return send_file(ZIP_PATH, as_attachment=True)
