b653826304ed08fc019da31afc3cbe4616cd2acc
kenbo-7net <104003646+kenbo-7net@users.noreply.github.com>
2025-07-18 23:41:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import zipfile
 from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
-from nsfw_mosaic import process_images  # 自作モジュール（存在前提）
+from nsfw_mosaic import process_images
 
 UPLOAD_FOLDER = 'uploads'
 OUTPUT_FOLDER = 'outputs'
@@ -12,37 +12,42 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
+# トップページ
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# モザイク処理とZIP返却
 @app.route('/process', methods=['POST'])
 def process():
-    # アップロードフォルダ初期化
-    for folder in [UPLOAD_FOLDER, OUTPUT_FOLDER]:
-        os.makedirs(folder, exist_ok=True)
-        for f in os.listdir(folder):
-            os.remove(os.path.join(folder, f))
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-    # アップロードファイルを保存
-    uploaded_files = request.files.getlist('images')
-    for file in uploaded_files:
-        if file.filename:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
+    for f in os.listdir(UPLOAD_FOLDER):
+        os.remove(os.path.join(UPLOAD_FOLDER, f))
+    for f in os.listdir(OUTPUT_FOLDER):
+        os.remove(os.path.join(OUTPUT_FOLDER, f))
+
+    files = request.files.getlist('images')
+    filepaths = []
+
+    for file in files:
+        filename = secure_filename(file.filename)
+        path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(path)
+        filepaths.append(path)
 
     # モザイク処理
-    process_images(UPLOAD_FOLDER, OUTPUT_FOLDER)
+    process_images(filepaths, OUTPUT_FOLDER)
 
-    # ZIPファイル作成
+    # ZIP化
     with zipfile.ZipFile(ZIP_PATH, 'w') as zipf:
-        for filename in os.listdir(OUTPUT_FOLDER):
-            zipf.write(os.path.join(OUTPUT_FOLDER, filename), filename)
+        for fname in os.listdir(OUTPUT_FOLDER):
+            fpath = os.path.join(OUTPUT_FOLDER, fname)
+            zipf.write(fpath, arcname=fname)
 
     return send_file(ZIP_PATH, as_attachment=True)
 
 if __name__ == '__main__':
-    # Renderで自動取得されるポート番号を取得して使う
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
-
