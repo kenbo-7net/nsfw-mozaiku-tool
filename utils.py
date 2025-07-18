1054022ce168e0d59a53b44a4bf1ee6767959fe6
kenbo-7net <104003646+kenbo-7net@users.noreply.github.com>
2025-07-18 19:46:50 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
 import cv2
 
-def mosaic_area(img, x1, y1, x2, y2, size):
-    w = x2 - x1
-    h = y2 - y1
-    roi = img[y1:y2, x1:x2]
+def load_image(image_path):
+    return cv2.imread(image_path)
+
+def apply_mosaic(image, x1, y1, x2, y2, size=30):
+    roi = image[y1:y2, x1:x2]
+    if roi.size == 0:
+        return image
     roi = cv2.resize(roi, (size, size), interpolation=cv2.INTER_LINEAR)
-    roi = cv2.resize(roi, (w, h), interpolation=cv2.INTER_NEAREST)
-    img[y1:y2, x1:x2] = roi
-    return img
+    roi = cv2.resize(roi, (x2 - x1, y2 - y1), interpolation=cv2.INTER_NEAREST)
+    image[y1:y2, x1:x2] = roi
+    return image
